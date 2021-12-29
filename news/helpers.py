a947ae24f2e324073d30c0eed8d546841d9ddd92
Bhushan Mohanraj <50306448+bhushanmohanraj@users.noreply.github.com>
2021-12-29 10:42:47 -0500

    Use assertion instead of raising `RuntimeError`

--- news/helpers.py
+++ news/helpers.py
@@ -10,11 +10,11 @@ def date_range(start: datetime.date, stop: datetime.date) -> list:
     Return the list of dates between the `start` and `stop` dates.
     """
 
-    if not start <= stop:
-        raise RuntimeError("The start date must be before the end date.")
+    assert start <= stop
 
     date_range = [start]
 
+    # The next date to add to the date range.
     next_date = start + datetime.timedelta(days=1)
 
     while next_date < stop:
