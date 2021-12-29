3bd333df4768a140dd90cdaf75d4d0f175b7186f
Bhushan Mohanraj <50306448+bhushanmohanraj@users.noreply.github.com>
2021-12-29 10:23:58 -0500

    Remove hurricane script and constants

--- news/__init__.py
+++ news/__init__.py
@@ -2,14 +2,6 @@
 Load and process headlines for climate change news articles.
 """
 
-# The hurricanes database file name.
-# This data is extracted (as of December 2021) from
-# https://products.climate.ncsu.edu/weather/hurricanes/database/
-HURRICANES_FILE_NAME = "hurricanes.csv"
-
-# The hurricanes folder name to store other hurricane data.
-HURRICANES_FOLDER_NAME = "hurricanes"
-
 # The article volume file name.
 # This data is extracted from the Google News feed.
 VOLUME_FILE_NAME = "volume.csv"
