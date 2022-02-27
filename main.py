f8c1e1a6fe6305da73f959c34ccdc8b9e8598137
Bhushan Mohanraj <50306448+bhushanmohanraj@users.noreply.github.com>
2022-02-27 17:03:23 -0500

    Add `main.py` script for loading volumes and titles.

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Load the volume and title for various queries.
+"""
+
+CLIMATE_CHANGE_QUERY = "climate change"
+
+NATURAL_DISASTER_QUERIES = [
+    "natural disaster",
+    "cyclone",
+    "hurricane",
+    "tornado",
+    "wildfire",
+    "flood",
+    "tsunami",
+    "drought",
+]
